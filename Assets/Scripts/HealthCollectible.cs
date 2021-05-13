f44d0c21b77cad50fc49d76111026501b2b33289
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2021-05-13 19:12:24 +0200

    break off early

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -5,15 +5,9 @@ public class HealthCollectible : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         var controller = other.GetComponent<RubyController>();
-        if (controller == null) return;
+        if (controller == null || controller.health >= controller.maxHealth) return;
 
-        controller.ChangeHealth(1);
+        controller.ChangeHealth(10);
         Destroy(gameObject);
-
-        if (controller.health < controller.maxHealth)
-        {
-            controller.ChangeHealth(1);
-            Destroy(gameObject);
-        }
     }
 }
